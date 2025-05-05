5821bce9bdfd2a81b3b508b741211ffbbb00b657
Kisalay Komal <100374713+Kisalay0298@users.noreply.github.com>
2025-05-05 16:45:21 +0530

    Time: 1086 ms (56.9%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,11 +1,23 @@
 # Write your MySQL query statement below
 
-select a.product_id, a.year as first_year, a.quantity, a.price
-from Sales as a
-inner join
-(
+-- select a.product_id, a.year as first_year, a.quantity, a.price
+-- from Sales as a
+-- inner join
+-- (
+--     select product_id, min(year) as year
+--     from Sales
+--     group by product_id
+-- ) as b
+-- on a.product_id=b.product_id and a.year=b.year
+
+
+-- another solution
+
+select product_id, year as first_year, quantity, price
+from Sales
+where(product_id, year)
+in(
     select product_id, min(year) as year
     from Sales
     group by product_id
-) as b
-on a.product_id=b.product_id and a.year=b.year
+)
