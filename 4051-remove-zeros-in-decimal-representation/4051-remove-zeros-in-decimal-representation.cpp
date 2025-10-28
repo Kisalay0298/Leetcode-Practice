2a182f4ee6d3e843b7de11a86f104dafd2271ec4
Kisalay Komal <100374713+Kisalay0298@users.noreply.github.com>
2025-10-28 18:53:23 +0530

    Time: 0 ms (100%), Space: 9.5 MB (13.51%) - LeetHub

---
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long removeZeros(long long n) {
+        string num = to_string(n);
+        string ans="";
+        for(int i=0;i<num.size();i++){
+            if(num[i]!='0'){
+                ans+=num[i];
+            }
+        }
+        return stoll(ans);
+    }
+};
